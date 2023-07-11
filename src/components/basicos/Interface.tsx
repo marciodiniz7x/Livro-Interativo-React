f78ac7411f3fb0486551251858dc6ed3ae01ad7c
Márcio Diniz <marcio.sccp10@gmail.com>
2023-07-11 16:28:47 -0300

    Updates

--- src/components/basicos/Interface.tsx
+++ src/components/basicos/Interface.tsx
@@ -4,7 +4,18 @@ export default function Interface() {
             <div className="interface">
                 <h1>Escolha o seu Caminho</h1>
                 <div className="capitulos-area">
-                    
+                    <div className="prologo">
+                        <h2>Prólogo</h2>
+                        <span>O SALGUEIRO ETÉREO</span>
+                    </div>
+                    <div className="capitulos-todos">
+                        <div className="ato1"></div>
+                        <div className="ato2"></div>
+                        <div className="ato3"></div>
+                        <div className="ato4"></div>
+                        <div className="ato5"></div>
+                        <div className="ato6"></div>
+                    </div>
                 </div>
             </div>
             
