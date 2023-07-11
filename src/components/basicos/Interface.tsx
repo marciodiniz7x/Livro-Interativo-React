40771d5c5f93bc1e98fdee539b896b4313ae1283
Márcio Diniz <marcio.sccp10@gmail.com>
2023-07-11 18:51:52 -0300

    Updates

--- src/components/basicos/Interface.tsx
+++ src/components/basicos/Interface.tsx
@@ -10,17 +10,29 @@ export default function Interface() {
                     </div>
                     <div className="capitulos-todos">
                         <div className="ato1 atos">
-                            <h2>Ato das Cavernas:</h2>
-                            <span>ARDRA, O ELEMENTISTA</span>
+                            <h2>Pré-história:</h2>
+                            <span>AARDRA, O ELEMENTISTA</span>
                         </div>
                         <div className="ato2 atos">
-                            <h2>Ato da Idade Antiga:</h2>
-                            <span>ARDRA, O ELEMENTISTA</span>
+                            <h2>Idade Antiga:</h2>
+                            <span>NIRVANA, A PORTADORA DA VIDA</span>
                         </div>
-                        <div className="ato3 atos"></div>
-                        <div className="ato4 atos"></div>
-                        <div className="ato5 atos"></div>
-                        <div className="ato6 atos"></div>
+                        <div className="ato3 atos">
+                            <h2>Idade Média:</h2>
+                            <span>ANIMA, O TELEPATA</span>
+                        </div>
+                        <div className="ato4 atos">
+                            <h2>Era Industrial:</h2>
+                            <span>IXIS, A RAINHA ELÉTRICA</span>
+                        </div>
+                        <div className="ato5 atos">
+                            <h2>Era Moderna:</h2>
+                            <span>GRAVITH, O MESTRE DO TEMPO</span>
+                        </div>
+                        <div className="ato6 atos">
+                            <h2>Futura Distante:</h2>
+                            <span>ETROS, O CAVALEIRO DA MORTE</span>
+                        </div>
                     </div>
                     <div className="prologo">
                         <h2>Ato Final</h2>
